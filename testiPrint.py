98cab2bb55946e6e8ccb0150948d5e45458ca881
ptjtammi <pekka.tj.tammi@student.jyu.fi>
2023-11-29 16:36:47 +0200

    Tää taas tekee juttuja csv tiedostolle. Tää printtaa uuteen tiedostoon kaiken siihen asti et tulee 15:30, mut jos siinä solussa on vielä dataa nii se tulee loppuu kuiteski

--- testiPrint.py
+++ testiPrint.py
@@ -1,10 +1,27 @@
-file_path = 'pyyttonRojekti\data3.log'  
+import csv
+import os
 
-try:
-    with open(file_path, 'r') as file:
-        first_line = file.readline()
-        print(first_line)
-except FileNotFoundError:
-    print(f"The file '{file_path}' was not found.")
-except Exception as e:
-    print(f"An error occurred: {str(e)}")
+
+file_path = 'file2.csv'
+stop_substring = '15:30'
+copy_file_path = "file4.csv"
+apu_taulukko = []
+apu_boolean = False
+with open(file_path, 'r', newline='') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        if not apu_boolean:
+            found_in_row = any(stop_substring in column_data for column_data in row)
+            if found_in_row:
+                apu_boolean = True
+                stop_column_index = row.index(next((column_data for column_data in row if stop_substring in column_data), None))
+                apu_taulukko.append(row[:stop_column_index + 1])
+                break  
+            else:
+                apu_taulukko.append(row)  
+
+print(apu_taulukko)
+
+with open(copy_file_path, 'w', newline='') as copyfile:
+    writer = csv.writer(copyfile)
+    writer.writerows(apu_taulukko)
